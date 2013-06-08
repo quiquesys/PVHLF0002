e0a7c6cdef048c4f41a81e5d93d74ab41e8bb8b9
quiquesys <quiquesys@gmail.com>
2013-06-08 11:35:53 -0600

    Primer commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace PVHLF0002
+{
+    partial class frm_PanelMensajes
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frm_PanelMensajes));
+            this.lblMensaje = new System.Windows.Forms.Label();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.tsslStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.statusStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblMensaje
+            // 
+            this.lblMensaje.AutoEllipsis = true;
+            resources.ApplyResources(this.lblMensaje, "lblMensaje");
+            this.lblMensaje.BackColor = System.Drawing.Color.Transparent;
+            this.lblMensaje.ForeColor = System.Drawing.Color.Red;
+            this.lblMensaje.Name = "lblMensaje";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsslStatus});
+            resources.ApplyResources(this.statusStrip1, "statusStrip1");
+            this.statusStrip1.Name = "statusStrip1";
+            // 
+            // tsslStatus
+            // 
+            this.tsslStatus.Name = "tsslStatus";
+            resources.ApplyResources(this.tsslStatus, "tsslStatus");
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.Controls.Add(this.tableLayoutPanel1);
+            this.panel1.Controls.Add(this.lblMensaje);
+            resources.ApplyResources(this.panel1, "panel1");
+            this.panel1.Name = "panel1";
+            // 
+            // tableLayoutPanel1
+            // 
+            resources.ApplyResources(this.tableLayoutPanel1, "tableLayoutPanel1");
+            this.tableLayoutPanel1.Controls.Add(this.richTextBox1, 0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.MenuText;
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            resources.ApplyResources(this.richTextBox1, "richTextBox1");
+            this.richTextBox1.ForeColor = System.Drawing.Color.Red;
+            this.richTextBox1.Name = "richTextBox1";
+            // 
+            // frm_PanelMensajes
+            // 
+            this.AllowDrop = true;
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.statusStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MinimizeBox = false;
+            this.Name = "frm_PanelMensajes";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.frm_PanelMensajes_Load);
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frm_PanelMensajes_FormClosing);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblMensaje;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel tsslStatus;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
+using System.Security.Permissions;
+
+namespace PVHLF0002
+{
+    public partial class frm_PanelMensajes : Form
+    {
+        System.Net.Sockets.TcpClient clientSocket; 
+        Thread tEscuchar;
+        Thread tConectar;
+        public frm_PanelMensajes()
+        {
+            InitializeComponent();
+            tEscuchar = new Thread(new ThreadStart(mEscuchar));
+            tConectar = new Thread(new ThreadStart(mConectar));
+        }      
+
+        private void frm_PanelMensajes_Load(object sender, EventArgs e)
+        {   
+            
+            richTextBox1.SelectionAlignment = HorizontalAlignment.Center;
+            CDI_P_CentrarLayout();
+            CheckForIllegalCrossThreadCalls = false;
+            try
+            {
+         
+                mConectar();
+            }
+            catch (SocketException se)
+            {
+                tsslStatus.Text = "Servidor de mensajes desconectado";
+                mDesconectar();
+            }
+        }
+        public void msg(string mesg)
+        {
+            lblMensaje.Text = mesg;
+            richTextBox1.Text=mesg;
+            
+            //.Text + Environment.NewLine + " >> " + mesg;
+            CDI_P_CentrarTexto(ref lblMensaje);
+        }
+        public void msgInicial(string mesg)
+        {
+            lblMensaje.Text = mesg;
+            richTextBox1.Text=mesg;
+            CDI_P_CentrarTexto(ref lblMensaje);
+        }
+       
+        private DataSet CDI_F_LeerXML()
+        {
+            DataSet ds = new DataSet();//Se crea un dataset
+            ds.ReadXml(Application.StartupPath+"\\configuracion.xml");//Dirección del archivo xml
+            return ds;
+        }
+        private void CDI_P_CentrarTexto(ref Label objeto)
+        {   
+            int heVentana=panel1.Size.Height;
+            int wiVentana=panel1.Size.Width;
+            int heObjeto = objeto.Size.Height;
+            int wiObjeto = objeto.Size.Width;
+            //Point pLocation = new Point(((wiVentana - wiObjeto) / 2),((heVentana-heObjeto)/2));
+            Point pLocation = new Point(((wiVentana - wiObjeto) / 2), heObjeto);
+            objeto.Location=pLocation;
+            //objeto.Left = ((wiVentana - wiObjeto) / 2);
+        }
+        private void CDI_P_CentrarLayout()
+        {
+            int heVentana = panel1.Size.Height;
+            int wiVentana = panel1.Size.Width;
+            tableLayoutPanel1.Size = new System.Drawing.Size(wiVentana, heVentana);
+        }
+
+        private void frm_PanelMensajes_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                mKillThread();
+            }
+            catch (Exception ex)
+            {
+                Console.Write(ex.ToString());
+            }
+        }
+        private void mConectar()
+        {
+            try
+            {
+                clientSocket = new System.Net.Sockets.TcpClient();
+                msgInicial("Cliente iniciado");
+                DataSet ds = new DataSet();
+                ds = CDI_F_LeerXML();
+                clientSocket.Connect(ds.Tables[0].Rows[0]["ip"].ToString(), int.Parse(ds.Tables[0].Rows[0]["puerto"].ToString()));
+                //clientSocket.Connect("172.17.7.31", 8888);
+                tsslStatus.Text = "Pantalla mensajes - Servidor Conectado ...";
+                tEscuchar.IsBackground = true;
+                tEscuchar.Start();                
+            }catch(Exception ex){
+                tsslStatus.Text = "Servidor de mensajes desconectado";
+                
+            }
+        }
+        private void mEscuchar()
+        {
+            while ((true))
+            {
+                try
+                {
+                    NetworkStream serverStream = clientSocket.GetStream();
+                    byte[] outStream = System.Text.Encoding.ASCII.GetBytes("true$");
+                    serverStream.Write(outStream, 0, outStream.Length);
+                    serverStream.Flush();
+
+                    byte[] inStream = new byte[10025];
+                    serverStream.Read(inStream, 0, (int)clientSocket.ReceiveBufferSize);
+                    string returndata = System.Text.Encoding.ASCII.GetString(inStream);
+                    msg(returndata);
+                }
+                catch (Exception se)
+                {
+                    tsslStatus.Text = "Servidor de mensajes desconectado";
+                    
+                }
+            }
+        }
+        private void mDesconectar()
+        {
+            tEscuchar.Abort();
+            clientSocket.Close();
+            clientSocket = null;
+            //mConectar();
+
+        }
+//        [SecurityPermissionAttribute(SecurityAction.Demand, ControlThread = true)]
+        private void mKillThread()
+        {   
+            tEscuchar.Abort(tEscuchar);
+        }
+    }
+}
+
